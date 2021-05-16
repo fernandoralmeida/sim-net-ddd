155c56b3e04dd63544f19bc6c66dbbbba7cec090
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-05-15 21:08:50 -0300

    up-15052021-2108

--- src/Sim.UI.Web/wwwroot/js/site.js
+++ src/Sim.UI.Web/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 100) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            document.getElementById('navbar_top').classList.add('shadow-sm');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            document.getElementById('navbar_top').classList.remove('shadow-sm');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
+// DOMContentLoaded  end
