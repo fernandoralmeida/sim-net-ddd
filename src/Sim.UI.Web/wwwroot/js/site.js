15a1bfaffec377f9b35433438f77249c9f5284d3
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-06-15 17:11:28 -0300

    up-15062021-1711

--- src/Sim.UI.Web/wwwroot/js/site.js
+++ src/Sim.UI.Web/wwwroot/js/site.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', function () {
         if (window.scrollY > 80) {
             document.getElementById('navbar_top').classList.add('fixed-top');
-            document.getElementById('navbar_top').classList.add('shadow-sm');
+            //document.getElementById('navbar_top').classList.add('shadow-sm');
             // add padding top to show content behind navbar
             navbar_height = document.querySelector('.navbar').offsetHeight;
             document.body.style.paddingTop = navbar_height + 'px';
         } else {
             document.getElementById('navbar_top').classList.remove('fixed-top');
-            document.getElementById('navbar_top').classList.remove('shadow-sm');
+            //document.getElementById('navbar_top').classList.remove('shadow-sm');
             // remove padding top from body
             document.body.style.paddingTop = '0';
         }
