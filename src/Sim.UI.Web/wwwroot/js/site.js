4fead261a20b4808933532dd0c946a6f929c6ac9
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-06-30 03:23:23 -0300

    up-30062021-0323

--- src/Sim.UI.Web/wwwroot/js/site.js
+++ src/Sim.UI.Web/wwwroot/js/site.js
@@ -45,6 +45,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var elemscollapsible = document.querySelectorAll('.collapsible');
     var instances = M.FormSelect.init(elemscollapsible, options);
     var instances = M.Collapsible.init(elemsselect, options);
+
+    var elemssidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elemssidenav, options);
 });
 
 // Or with jQuery
@@ -57,4 +60,23 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $('.collapsible').collapsible();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$('.collapsible').collapsible();
+$('.collapsible.expandable').collapsible({
+    accordion: false
 });
