9de989a23aed8b8be90428d77e939ac8be3e046a
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-07-13 11:09:02 -0300

    up-13072021-1107

--- src/Sim.UI.Web/Pages/Atendimento/Edit.cshtml.cs
+++ src/Sim.UI.Web/Pages/Atendimento/Edit.cshtml.cs
@@ -1,18 +1,174 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sim.UI.Web.Pages.Atendimento
 {
+    using Sim.Application.SDE.Interface;
+    using Sim.Application.Shared.Interface;
+    using Sim.Cross.Identity;
+    using Sim.Domain.SDE.Entity;
+    using Sim.Domain.Shared.Entity;
+
+
     [Authorize]
     public class EditModel : PageModel
     {
-        public void OnGet()
+        //private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IAppServiceAtendimento _appServiceAtendimento;
+        private readonly IAppServicePessoa _appServicePessoa;
+        private readonly IAppServiceEmpresa _appServiceEmpresa;
+        private readonly IAppServiceSetor _appServiceSetor;
+        private readonly IAppServiceCanal _appServiceCanal;
+        private readonly IAppServiceServico _appServiceServico;
+
+        public EditModel(IAppServiceAtendimento appServiceAtendimento,
+            IAppServicePessoa appServicePessoa,
+            IAppServiceEmpresa appServiceEmpresa,
+            IAppServiceCanal appServiceCanal,
+            IAppServiceServico appServiceServico,
+            IAppServiceSetor appServiceSetor)
         {
+            _appServiceAtendimento = appServiceAtendimento;
+            _appServicePessoa = appServicePessoa;
+            _appServiceEmpresa = appServiceEmpresa;
+            _appServiceCanal = appServiceCanal;
+            _appServiceServico = appServiceServico;
+            _appServiceSetor = appServiceSetor;
+            //_userManager = userManager;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public InputModel Input { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [Required(ErrorMessage = "Selecione o setor do atendimento!")]
+        [BindProperty(SupportsGet = true)]
+        public string GetSetor { get; set; }
+
+        public SelectList Setores { get; set; }
+
+        public string GetServico { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ServicosSelecionados { get; set; }
+
+        private async Task OnLoad()
+        {
+            var set = Task.Run(() => _appServiceSetor.List());
+            await set;
+
+            if (set.Result != null)
+            {
+                Setores = new SelectList(set.Result, nameof(Setor.Nome), nameof(Setor.Nome), null);
+            }
+        }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            await OnLoad();
+
+            //var user = await _userManager.GetUserAsync(User);
+
+            var atendimemnto_ativio = _appServiceAtendimento.GetAtendimento((Guid)id);
+
+            if (atendimemnto_ativio == null)
+            {
+                StatusMessage = "Erro inesperado, tente novamente!";
+                return RedirectToPage("./Index");
+            }
+
+            Input = new()
+            {
+                Id = atendimemnto_ativio.Id,
+                Protocolo = atendimemnto_ativio.Protocolo,
+                Data = atendimemnto_ativio.Data,
+                DataF = atendimemnto_ativio.DataF,
+                Setor = atendimemnto_ativio.Setor,
+                Canal = atendimemnto_ativio.Canal,
+                Servicos = atendimemnto_ativio.Servicos,
+                Descricao = atendimemnto_ativio.Descricao,
+                Status = atendimemnto_ativio.Status,
+                Ultima_Alteracao = atendimemnto_ativio.Ultima_Alteracao,
+                Ativo = atendimemnto_ativio.Ativo,
+                Owner_AppUser_Id = atendimemnto_ativio.Owner_AppUser_Id,
+                Pessoa = atendimemnto_ativio.Pessoa,
+                Empresa = atendimemnto_ativio.Empresa                
+            };
+
+            var set = Task.Run(() => _appServiceSetor.List());
+            await set;
+
+            if (set.Result != null)
+            {
+                Setores = new SelectList(set.Result, nameof(Setor.Nome), nameof(Setor.Nome), null);
+            }
+
+            ServicosSelecionados = Input.Servicos;
+
+            return Page();
+        }
+
+        public JsonResult OnGetCanais()
+        {
+            return new JsonResult(_appServiceCanal.GetByOwner(GetSetor));
+        }
+
+        public JsonResult OnGetServicos()
+        {
+            return new JsonResult(_appServiceServico.GetByOwner(GetSetor));
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            try
+            {
+
+                if (Input.Servicos == null || Input.Servicos == string.Empty)
+                {
+                    StatusMessage = "Erro: " + "Selecione um serviço ou mais!";
+                    await OnLoad();
+                    return RedirectToPage();
+                }
+
+                //var user = await _userManager.GetUserAsync(User);
+
+                var t = Task.Run(() =>
+                {
+                    //var atendimemnto_ativio = _appServiceAtendimento.AtendimentoAtivo(user.Id).FirstOrDefault();
+
+                    var atold = _appServiceAtendimento.GetById(Input.Id);
+                    atold.DataF = DateTime.Now;
+                    atold.Setor = Input.Setor; //GetSetor;
+                    atold.Canal = Input.Canal;  //GetCanal;
+                    atold.Servicos = ServicosSelecionados; //MeusServicos;
+                    atold.Descricao = Input.Descricao;
+                    atold.Status = "Finalizado";
+                    atold.Ultima_Alteracao = DateTime.Now;
+                    _appServiceAtendimento.Update(atold);
+
+                });
+
+                await t;
+
+                return RedirectToPage("./Index");
+
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = "Erro: " + ex.Message;
+                return Page();
+            }
+
         }
     }
 }
