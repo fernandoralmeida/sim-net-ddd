e41ae559e12cbc7a2b4af445caccf911fd50be67
Fernando Almeida <fernando.ralmeida@outlook.com>
2022-05-12 17:14:41 -0300

    up-20220512-1714

---
+++ src/Sim.UI.Web/Pages/Pat/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sim.UI.Web.Pages.Pat
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Sim.UI.Web/Pages/Pat/PatNav.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Sim.UI.Web.Pages.Pat
+{
+    public static class PatNav
+    {
+        public static string Inicio => "Index";
+        public static string RaeLancados => "Rae.Lancados";
+        public static string InicioNavClass(ViewContext viewContext) => PageNavClass(viewContext, Inicio);
+        public static string AtendiementoNavClass(ViewContext viewContext) => PageNavClass(viewContext, RaeLancados);
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
